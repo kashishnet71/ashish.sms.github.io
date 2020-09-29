7abf76310bcd2623de9de15ec466c740f5a09e13
kashishnet71 <72105344+kashishnet71@users.noreply.github.com>
2020-09-30 02:47:02 +0530

    Add files via upload

---
+++ sms/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Management System</title>
+</head>
+<body>
+<h5 align="right" style="margin-right: 20px: "><a href="login.php">Admin Login</a></h5>
+<h1 align="center">Welcome to Student Management System</h1>
+<form method="post" action="index.php">
+<table style="width: 30%:" align="center" border="1">
+	<tr>
+		<td colspan="2" align="center">Student Information</td>
+	</tr>
+	<tr>
+		<td align="right">Choose Standard</td>
+		<td>
+			<select name="std">
+				<option value="1">1st</option>
+                <option value="2">2nd</option>
+                <option value="3">3rd</option>
+                <option value="4">4th</option>
+                <option value="5">5th</option>
+                <option value="6">6th</option>
+
+			</select>
+		</td></tr>
+		<tr>
+			<td align="left">Enter Rollno</td>
+			<td><input type="text" name="rollno"></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center"><input type="submit" name="submit" value="Show Info"></td>
+		</tr>
+</table>
+</form>
+</body>
+</html>
+<?php
+if (isset($_POST['submit']))
+ {
+ 	$standard= $_POST['std'];
+ 	$rollno= $_POST['rollno'];
+
+ 	include('dbcon.php');
+ 	include('admin/function.php');
+ 	showdetails($standard,$rollno);
+ } 
+
+
+
+
+
+?>
